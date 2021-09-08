8a5c335b3e57295b638d285937ebef40e0820f42
ryan-byrnes <ryn_byrnes@yahoo.com>
2021-09-08 05:11:53 +0000

    created db, testing post route allowing user to input training maxes

--- database/schema.sql
+++ database/schema.sql
@@ -5,3 +5,60 @@ set client_min_messages to warning;
 drop schema "public" cascade;
 
 create schema "public";
+
+CREATE TABLE "public"."users" (
+	"userId" serial NOT NULL,
+	"yearsExperience" integer NOT NULL,
+	"sportTrainingFor" TEXT NOT NULL,
+	"injuries" TEXT NOT NULL,
+	"daysPerWeek" integer NOT NULL,
+	"trainingDuration" TEXT NOT NULL,
+	CONSTRAINT "users_pk" PRIMARY KEY ("userId")
+) WITH (
+  OIDS=FALSE
+);
+
+
+
+CREATE TABLE "public"."prs" (
+	"weight" integer NOT NULL,
+	"reps" integer NOT NULL,
+	"userId" integer NOT NULL,
+	"exerciseId" integer NOT NULL
+) WITH (
+  OIDS=FALSE
+);
+
+
+
+CREATE TABLE "public"."trainingLog" (
+	"userId" integer NOT NULL,
+	"date" timestamp with time zone NOT NULL,
+	"sets" integer NOT NULL,
+	"reps" integer NOT NULL,
+	"weight" integer NOT NULL,
+	"exerciseId" integer NOT NULL
+) WITH (
+  OIDS=FALSE
+);
+
+
+
+CREATE TABLE "public"."exerciseList" (
+	"exerciseId" serial NOT NULL,
+	"exercise" TEXT NOT NULL UNIQUE,
+	"description" TEXT,
+	"videoPath" TEXT,
+	CONSTRAINT "exerciseList_pk" PRIMARY KEY ("exerciseId")
+) WITH (
+  OIDS=FALSE
+);
+
+
+
+
+ALTER TABLE "prs" ADD CONSTRAINT "prs_fk0" FOREIGN KEY ("userId") REFERENCES "users"("userId");
+ALTER TABLE "prs" ADD CONSTRAINT "prs_fk1" FOREIGN KEY ("exerciseId") REFERENCES "exerciseList"("exerciseId");
+
+ALTER TABLE "trainingLog" ADD CONSTRAINT "trainingLog_fk0" FOREIGN KEY ("userId") REFERENCES "users"("userId");
+ALTER TABLE "trainingLog" ADD CONSTRAINT "trainingLog_fk1" FOREIGN KEY ("exerciseId") REFERENCES "exerciseList"("exerciseId");
--- server/index.js
+++ server/index.js
@@ -2,12 +2,51 @@ require('dotenv/config');
 const express = require('express');
 const errorMiddleware = require('./error-middleware');
 const staticMiddleware = require('./static-middleware');
+const pg = require('pg');
+const ClientError = require('./client-error');
+
+const db = new pg.Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false
+  }
+});
+
+const jsonMiddleware = express.json();
 
 const app = express();
+
+app.use(jsonMiddleware);
 
 app.use(staticMiddleware);
 
 app.use(errorMiddleware);
+
+app.post('/api/pr', (req, res, next) => {
+  const { exercise, reps, weight } = req.body;
+
+  if (!exercise || !reps || !weight) {
+    throw new ClientError(400, 'Exercise, reps, and weight are required fields.');
+  }
+  const sql = `
+  insert into "prs" ("reps", "weight")
+  values ($1, $2)
+  returning *;
+
+  insert into "prs" ("exerciseId")
+  select "exerciseId"
+  from "exerciseList"
+  where "exercise" = $3
+  `;
+
+  const params = [reps, weight, exercise];
+
+  db.query(sql, params)
+    .then(result => {
+      res.json(result.rows[0]);
+    })
+    .catch(err => next(err));
+});
 
 app.listen(process.env.PORT, () => {
   // eslint-disable-next-line no-console
