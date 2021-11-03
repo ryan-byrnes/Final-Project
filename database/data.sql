b91d66a17b7112acc7803dec070adcd9a31999df
Ryan Byrnes <ryn_byrnes@yahoo.com>
2021-11-03 13:16:20 -0700

    added videopaths

--- database/data.sql
+++ database/data.sql
@@ -1,56 +1,56 @@
-insert into "exerciseList" ("exercise")
-values ('Back Squat');
+insert into "exerciseList" ("exercise", "videoPath")
+values ('Back Squat', 'https://www.youtube.com/embed/u5s1sCtFrkI');
 
-insert into "exerciseList" ("exercise")
-values ('Bench Press');
+insert into "exerciseList" ("exercise", "videoPath")
+values ('Bench Press', 'https://www.youtube.com/embed/wHUXLaNoXqo');
 
-insert into "exerciseList" ("exercise")
-values ('Power Clean');
+insert into "exerciseList" ("exercise", "videoPath")
+values ('Power Clean', 'https://www.youtube.com/embed/_O5BacPSZ9w');
 
-insert into "exerciseList" ("exercise")
-values ('Deadlift');
+insert into "exerciseList" ("exercise", "videoPath")
+values ('Deadlift', 'https://www.youtube.com/embed/uhCdgacVInA');
 
-insert into "exerciseList" ("exercise")
-values ('Clean');
+insert into "exerciseList" ("exercise", "videoPath")
+values ('Clean', 'https://www.youtube.com/embed/wlGwi-kEDpI');
 
-insert into "exerciseList" ("exercise")
-values ('Snatch');
+insert into "exerciseList" ("exercise", "videoPath")
+values ('Power Snatch', 'https://www.youtube.com/embed/SMg45PETa2w');
 
-insert into "exerciseList" ("exercise")
-values ('Bent Row');
+insert into "exerciseList" ("exercise", "videoPath")
+values ('Bent Row', 'https://www.youtube.com/embed/KqcAOsF0Zgk');
 
-insert into "exerciseList" ("exercise")
-values ('Push Press');
+insert into "exerciseList" ("exercise", "videoPath")
+values ('Push Press', 'https://www.youtube.com/embed/UfeStlQM7y8');
 
-insert into "exerciseList" ("exercise")
-values ('Strict Press');
+insert into "exerciseList" ("exercise", "videoPath")
+values ('Strict Press', 'https://www.youtube.com/embed/9NEQRCStCQ0');
 
-insert into "exerciseList" ("exercise")
-values ('Power Snatch');
+insert into "exerciseList" ("exercise", "videoPath")
+values ('Snatch Deadlift', 'https://www.youtube.com/embed/JAC2jKmk1q4');
 
-insert into "exerciseList" ("exercise")
-values ('Clean Pull');
+insert into "exerciseList" ("exercise", "videoPath")
+values ('Clean Pull', 'https://www.youtube.com/embed/i-X0S7CcaYw');
 
-insert into "exerciseList" ("exercise")
-values ('Snatch Pull');
+insert into "exerciseList" ("exercise", "videoPath")
+values ('Snatch Pull', 'https://www.youtube.com/embed/PzDWLcyvudg');
 
-insert into "exerciseList" ("exercise")
-values ('RDL');
+insert into "exerciseList" ("exercise", "videoPath")
+values ('RDL', 'https://www.youtube.com/embed/AE-19yPt_Jo');
 
-insert into "exerciseList" ("exercise")
-values ('Split Squat');
+insert into "exerciseList" ("exercise", "videoPath")
+values ('Split Squat', 'https://www.youtube.com/embed/90n0R9iZrTo');
 
-insert into "exerciseList" ("exercise")
-values ('Good Morning');
+insert into "exerciseList" ("exercise", "videoPath")
+values ('Good Morning', 'https://www.youtube.com/embed/UwHpZ72e_To');
 
-insert into "exerciseList" ("exercise")
-values ('Nordic Curls');
+insert into "exerciseList" ("exercise", "videoPath")
+values ('Nordic Curls', 'https://www.youtube.com/embed/2Q5E05-Zci0');
 
-insert into "exerciseList" ("exercise")
-values ('Split Jerk');
+insert into "exerciseList" ("exercise", "videoPath")
+values ('Split Jerk', 'https://www.youtube.com/embed/oC2yhrp0H90' );
 
-insert into "exerciseList" ("exercise")
-values ('Biceps Curl');
+insert into "exerciseList" ("exercise", "videoPath")
+values ('Biceps Curl', 'https://www.youtube.com/embed/BMs7khra4tY');
 
 insert into "users" ("name")
 values ('Ryan');
