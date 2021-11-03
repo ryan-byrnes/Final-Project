cf4beced1141764d65233e8bcafd7ac9a729b45e
Ryan Byrnes <ryn_byrnes@yahoo.com>
2021-11-02 18:49:53 -0700

    figured out how to embed youtube videos for my exercise videos

--- client/components/exercise-information.jsx
+++ client/components/exercise-information.jsx
@@ -13,6 +13,7 @@ export default class ExerciseInformation extends React.Component {
       <div className="modal">
         <div className="modal-content position-relative overflow-scroll">
           <h1 className="margin-top-10 margin-bottom-10">{this.state.exercise}</h1>
+          <iframe className="border-radius-5" width="400" height="250" src="https://www.youtube.com/embed/_O5BacPSZ9w" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
           <a className="color-red position-absolute close-size" onClick={this.props.handleClose}><i className="fas fa-times"></i></a>
         </div>
       </div>
